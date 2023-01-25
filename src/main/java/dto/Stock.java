24d53f3c9dff5736a2a8271ef15612dfc47b8626
nishizawareo <r.nishizawa.sys22@morijyobi.ac.jp>
2023-01-25 12:13:15 +0900

    Stock.javaの追加

---
+++ src/main/java/dto/Stock.java
@@ -0,0 +1,28 @@
+package dto;
+
+public class Stock {
+	private String name;
+	private int num;
+	
+	public Stock(String name, int num) {
+		super();
+		this.name = name;
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
