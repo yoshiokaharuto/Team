192369079637469c638f9e9f084d86c51ce7458f
igarashi <t.igarashi.sys22@morijyobi.ac.jp>
2023-01-26 14:49:03 +0900

    備品登録を追加

---
+++ src/main/java/servlet/success.java
@@ -0,0 +1,60 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.StockDAO;
+import dto.Stock;
+
+/**
+ * Servlet implementation class success
+ */
+@WebServlet("/success")
+public class success extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public success() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	request.setCharacterEncoding("UTF-8");
+		
+		// フォーム入力内容の取得
+		String name = request.getParameter("name");
+		int num = Integer.parseInt(request.getParameter("num"));
+		
+		
+		// 入力された情報を元にインスタンスを生成
+		Stock bihin = new Stock(name,num);
+
+		int result = StockDAO.Registersuccess(bihin);
+		if(result == 1) {
+		String view = "WEB-INF/view/success.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+		dispatcher.forward(request, response);
+	}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
