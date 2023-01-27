e13c6c0041d4c24ea6f1e58a54b4065e0160fc6b
nishizawareo <r.nishizawa.sys22@morijyobi.ac.jp>
2023-01-27 11:20:59 +0900

    setString=>setIntへ変更

---
+++ src/main/java/dao/StockDAO.java
@@ -0,0 +1,53 @@
+package dao;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import dto.Stock;
+
+public class StockDAO {
+
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	    URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+	    return DriverManager.getConnection(dbUrl, username, password);
+	}
+	
+	//在庫登録
+	public static int RegisterStock(Stock stock) {
+		String sql = "INSERT INTO stock VALUES(name,num)";
+		int result = 0;
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1, stock.getName());
+			pstmt.setInt(2, stock.getNum());
+			
+
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件更新しました。");
+		}
+		return result;
+	}
+	
+}
