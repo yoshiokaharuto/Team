50ac1a7f6bc66dc756fa09de89a8724d89d20c91
h.yoshioka <h.yoshioka.sys22@morijyobi.ac.jp>
2023-01-27 11:51:08 +0900

    RegisterAccFormServletの修正

--- src/main/java/servlet/RegisterAccFormServlet.java
+++ src/main/java/servlet/RegisterAccFormServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class registerServlet
  */
-@WebServlet("/registerServlet")
+@WebServlet("/RegisterAccFormServlet")
 public class RegisterAccFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
